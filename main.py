c250ee81bfabf7f9b784a7f1bd183bb71d610b05
Lance Tsai <act762@msn.com>
2022-10-12 14:02:23 +0800

    2022-10

--- main.py
+++ main.py
@@ -18,3 +18,12 @@ backtest_initial_cash回测初始资金
 backtest_commission_ratio回测佣金比例
 backtest_slippage_ratio回测滑点比例
 """
+
+if __name__ == "__main__":
+    run(
+        strategy_id=os.environ.get("strategy_id", None),
+        filename="main.py",
+        mode=MODE_LIVE,
+        token=os.environ.get("token", None),
+    )
+
